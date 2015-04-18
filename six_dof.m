cbd9161520693159f9bd6ff09193ae75971f6253
klund1 <klund@hmc.edu>
2015-04-17 20:59:09 -0700

    actually added 6dof conversion (header only)

---
+++ six_dof.m
@@ -0,0 +1,21 @@
+function [ x, y, z ] = six_dof(ax,ay,az,gx,gy,gz)
+%
+% This function takes acceleration and angular velocity data and produces
+% x,y,z cartesian coordinates
+%
+% NOTE: This function asigns a starting position of (0,0,0) and initial 
+%       orientation along the x-axis
+%
+% INPUTS:
+%     - ax: acceleration along the x-axis
+%     - ay: acceleration along the y-axis
+%     - az: acceleration along the z-axis
+%
+% OUTPUTS:
+%     - gx: angular acceleration about the x-axis
+%     - gy: angular acceleration about the y-axis
+%     - gz: angular acceleration about the z-axis
+
+
+end
+
