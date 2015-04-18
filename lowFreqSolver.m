2b7a71d832f5a6935f91ddf66f65410985888f3b
AlecRBGriffith <agriffith@hmc.edu>
2015-04-17 22:00:21 -0700

    Version done

--- lowFreqSolver.m
+++ lowFreqSolver.m
@@ -1,7 +1,6 @@
 function [c v] = lowFreqSolver(t1, t2, d1, d2 )
-%UNTITLED Summary of this function goes here
-%   Detailed explanation goes here
-
-
+    result =   [t1 t1; t2 -t2] \ [d1 ; d2];
+    c = result(1);
+    v = result(2);
 end
 
