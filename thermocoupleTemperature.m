302760b45e67df08c185010309a865fb156804f0
AlecRBGriffith <agriffith@hmc.edu>
2015-04-17 23:30:52 -0700

    Fixed thermocouple function to work correctly)

--- thermocoupleTemperature.m
+++ thermocoupleTemperature.m
@@ -1,6 +1,9 @@
-function temp = thermocoupleTemperature( thermo_mV, CJC )
+function temp = thermocoupleTemperature( thermoV, CJC )
 % Need extra function or to add in here reversing the gain and 
 % voltage offset we have for this to work with the datalogger.
+vOffset = 4.04504;
+gain = (1+100/.560);
+thermo_mV = themoV/gain - vOffset;
 A = importdata('ThermoCoupleTable.txt');
 mapT = -150:1:149;
 temp = interp1(A,mapT,thermo_mV)+CJC;
