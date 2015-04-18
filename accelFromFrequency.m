c83b3d9278df286aa5f3fa93a16a8caadd217e4a
AlecRBGriffith <agriffith@hmc.edu>
2015-04-17 22:27:56 -0700

    formatted thermocouple table to be more usable, vectorized the lowFreqSolver

--- accelFromFrequency.m
+++ accelFromFrequency.m
@@ -1,5 +1,5 @@
 function acceleration = accelFromFrequency( frequencyBase, frequnecyShifted,c,v)
-    d = 0.2 % Hard code a distance here
+    d = 0.2; % Hard code a distance here
     gain  = frequencyShifted./frequencyBase;
     a = (1-g.^2).*(c-v).^2./(2*d);
 end
--- lowFreqSolver.m
+++ lowFreqSolver.m
@@ -1,6 +1,8 @@
 function [c v] = lowFreqSolver(t1, t2, d1, d2 )
-    result =   [t1 t1; t2 -t2] \ [d1 ; d2];
-    c = result(1);
-    v = result(2);
+    for i = 1:length(t1)
+        result =   [t1(i) t1(i); t2(i) -t2(i)] \ [d1(i) ; d2(i)];
+        c(i) = result(1);
+        v(i) = result(2);
+    end 
 end
 
