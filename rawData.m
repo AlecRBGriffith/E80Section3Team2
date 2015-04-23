4413e10b24b5f64a2986b8c9fcb643576edb60f2
klund1 <klund@hmc.edu>
2015-04-22 21:05:30 -0700

    added comments to rawData

--- rawData.m
+++ rawData.m
@@ -1,10 +1,15 @@
 function rawData(filename)
-% rawData.m
+% this function plots the data from a .DAT data file produced by the
+% datalogger
+% 
+% Inputs:
+%     - filename: a string, the full filename of the input .DAT file
 
-%Load the input file using the provided function
+% Load the input file using the provided function
 [X,Y] = ReadBinaryFileTX(filename,14,300000,3.3);
 disp('Loaded data')
 
+% plot all 14 channels in a single subplot
 for i = 1:14
     subplot(3,5,i)
     plot(X(:,i),Y(:,i))
