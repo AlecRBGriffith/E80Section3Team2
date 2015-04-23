4bb8fe7c614a67ba25a9e4dcafde30a9ef68de31
klund1 <klund@hmc.edu>
2015-04-22 21:21:15 -0700

    added comments to thermistor.m

--- thermistor.m
+++ thermistor.m
@@ -1,17 +1,30 @@
 function data = thermistor(inputdata)
-% applies steinhart-hart function
-R = 100000;             % bottom of voltage divider
-Rref = 100000;          % ref resistance of thermistor
-dat1 = R*inputdata;     % this makes vectorizing easier for my dumb brain
-dat2 = 3.3-inputdata;
-Tr = dat1./dat2;
+% this function converts voltages from the thermistor into temperature
+% readings
+%
+% Inputs:
+%     - inputdata: a vector of thermistor voltages
+%
+% Outputs:
+%     - data: a vector of calculated temeratures
+%
+
+R = 100000;    % bottom of voltage divider
+Rref = 100000; % ref resistance of thermistor
+
+% resistance of the thermocouple
+Rt = (R*inputdata)./(3.3-inputdata);
+
+% steinhart-hart constants for our thermocouple
 A = 3.354e-3;
 B = 2.460e-4;
 C = 3.405e-6;
 D = 1.034e-7;
 
-data = (A + B*log(Tr/Rref) + C*log(Tr/Rref).^2 + D*log(Tr/Rref).^3).^-1;
+% apply the steinhart-hart function to find temerature
+data = (A + B*log(Rt/Rref) + C*log(Rt/Rref).^2 + D*log(Rt/Rref).^3).^-1;
 
+% convert to deg C
 data = data-273.15;
 
 
